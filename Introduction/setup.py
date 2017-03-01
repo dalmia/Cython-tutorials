7bde88f87482bf0c8f65439fe756268b5cea6e28
Aman Dalmia <amandalmia18@gmail.com>
2017-03-01 20:02:59 +0530

    Introduction - check_speed

--- Introduction/setup.py
+++ Introduction/setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-  name = "Hello world",
-  ext_modules = cythonize("hello.pyx")
+  name = "Check speed",
+  ext_modules = cythonize("check_speed.pyx")
 )
